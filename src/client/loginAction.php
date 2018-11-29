d1a79f6886fa63064096e29fa882aab16e990be5
CrZ25 <chrischeung97@gmail.com>
2018-11-29 13:46:35 -0800

    login sqlsrv

--- src/client/loginAction.php
+++ src/client/loginAction.php
@@ -7,35 +7,46 @@ if (!isset($_SESSION['username'])) {
         if (isset($_POST["username"]) && isset($_POST["password"])) {
             include 'include/db_credentials.php';
 
-            $connection = mysqli_connect($host, $user, $password, $database);
-            $error      = mysqli_connect_error();
-            if ($error != null) {
+            // $connection = mysqli_connect($host, $user, $password, $database);
+            // $error      = mysqli_connect_error();
+
+            $con = sqlsrv_connect($server, $connectionInfo);
+
+            /*if ($error != null) {
                 $output = "<p>Unable to connect to database!</p>";
                 exit($output);
+            } */
+            if ($con === false ) {
+                die( print_r( sqlsrv_errors(), true));
             } else {
                 echo "here";
-                $sql     = "SELECT username, password FROM user WHERE username='" . $_POST['username'] . "';";
-                $results = mysqli_query($connection, $sql);
-                if ($row = mysqli_fetch_assoc($results)) {
-                    if ($_POST['password'] == $row['password']) {
+                $sql = "SELECT username, password FROM user WHERE username='" . $_POST['username'] . "';";
+                $pstmt = sqlsrv_query($con, $sql, array());
+
+                // $results = mysqli_query($connection, $sql);
+                // if ($row = mysqli_fetch_assoc($results)) {
+                while ($rst = sqlsrv_fetch_array( $pstmt, SQLSRV_FETCH_ASSOC)) {
+                    if ($_POST['password'] == $rst['password']) {
                         //Update session Superglobal
                         $_SESSION['username'] = $_POST['username'];
                         //Release Values
-                        mysqli_free_result($results);
-                        mysqli_close($connection);
+                        // mysqli_free_result($results);
+                        // mysqli_close($connection);
+                        sqlsrv_free_stmt($pstmt2);
+                        sqlsrv_close($con);
                         //redirect
                         header("Location: index.php");
                         die();
                     }
                 }
-                mysqli_free_result($results);
+                sqlsrv_free_stmt($pstmt2);
             }
-            mysqli_close($connection);
+            sqlsrv_close($con);
         }
     }
     //Data bad
-    header("Location: login.php");
-    die();
+    // header("Location: login.php");
+    // die();
 } else {
     //we logged in already silly goose
     header("Location: index.php");
