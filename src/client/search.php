e0dcd58470548f853d33fcad8f8d44c6ee1ccad1
CrZ25 <chrischeung97@gmail.com>
2018-11-29 22:35:10 -0800

    search functionality

--- src/client/search.php
+++ src/client/search.php
@@ -30,7 +30,8 @@
           <div class="titleCol middleCol">
             <div class="midTopFlex">
               <h2>Price:</h2>
-              <h2>Colour:</h2>
+              <h2>Drivetrain:</h2>
+              <h2>Engine:</h2>
             </div>
           </div>
           <!-- <div class="titleCol rightCol">
@@ -39,6 +40,7 @@
         </div>
 
         <?php
+          error_reporting(E_ALL);
           include './include/db_credentials.php';
 
           $connection = mysqli_connect($host, $user, $password, $database);
@@ -54,153 +56,63 @@
 
           if ($name == "") {
             echo("<h2>All Vehicles</h2>");
-            $sql = "SELECT year, make, model, price, description, productPic FROM Vehicle";
+            $sql = "SELECT year, make, model, price, description, productPic, COUNT(CommentsOn.vehicleID), drivetrain, engine, Vehicle.vehicleID FROM Vehicle LEFT OUTER JOIN CommentsOn ON Vehicle.vehicleID=CommentsOn.vehicleID GROUP BY vehicleID ORDER BY vehicleID DESC LIMIT 10";
           } else {
             echo("<h2>Vehicles containing '" . $name . "'</h2>");
             $hasParameter = true;
-            $sql = "SELECT year, make, model, price, description, productPic, SUM(commentID), drivetrain, engine FROM Vehicle NATURAL JOIN CommentsOn WHERE productName LIKE %'$name'% GROUP BY vehicleID";
+            $sql = "SELECT year, make, model, price, description, productPic, COUNT(CommentsOn.vehicleID), drivetrain, engine, Vehicle.vehicleID, transmission FROM Vehicle LEFT OUTER JOIN CommentsOn ON Vehicle.vehicleID=CommentsOn.vehicleID WHERE (make LIKE '%$name%' OR year LIKE '%$name%' OR model LIKE '%$name%' OR drivetrain LIKE '%$name%' OR engine LIKE '%$name%' OR transmission LIKE '%$name%')  GROUP BY vehicleID";
           }
+          // (year, make, model, drivetrain, engine, transmission)
+          // SELECT year, make, model, price, description, productPic, COUNT(CommentsOn.vehicleID), drivetrain, engine, Vehicle.vehicleID FROM Vehicle LEFT OUTER JOIN CommentsOn ON Vehicle.vehicleID=CommentsOn.vehicleID WHERE make LIKE tacoma GROUP BY vehicleID;
+          // SELECT year, make, model, price, description, productPic, COUNT(CommentsOn.vehicleID), drivetrain, engine, Vehicle.vehicleID FROM Vehicle LEFT OUTER JOIN CommentsOn ON Vehicle.vehicleID=CommentsOn.vehicleID GROUP BY vehicleID;
 
-          if($connection -> connect_error) {
+
+          if ($connection -> connect_error) {
             die("Connection failed: " . $connection -> connect_error);
           }
-          echo "Connected to Server."; 
+          // echo "Connected to Server."; 
           if ($error != null) {
             $output = "<p>Unable to connect to database!</p>";
             exit($output);
           } else {
-            echo "Connected to Database.";
+          
             if ($results = mysqli_query($connection, $sql)) {
+              // echo "in results";
               while ($row = mysqli_fetch_row($results)) {
+
+                $year = $row[0]; 
+                $make = $row[1]; 
+                $model = $row[2]; 
+                $price = $row[3]; 
+                $description = $row[4]; 
+                $productPic = $row[5];
+                $numComments= $row[6]; 
+                $drivetrain = $row[7]; 
+                $engine = $row[8]; 
+                $vehicleID = $row[9];
+                $vehiclePicStr = $year."-".$make."-".$model;
+                $vehicleName = $year." ".$make." ".$model;
+
                 echo '<div class="searchEntry"><div class="searchCol leftCol"><div class="thumbContainer"><a href="product.php">';
-                $vehiclePicStr = $row['0']."-".$row['1']."-".$row['2'];
-                echo "<img src='images/'$vehiclePicStr'.jpg'></a></div>";
-                $vehicleName = $row['0']." ".$row['1']." ".$row['2'];
-                echo "<a href='product.php' class='searchLink'>'$vehicleName'</a></div>";
+                echo "<img src='./images/$vehiclePicStr.jpg'></a></div>";
+                echo "<a href='product.php?id='$vehicleID' class='searchLink'>$vehicleName</a></div>";
                 echo '<div class="searchCol middleCol"><div class="midTopFlex"><div class="searchPrice">';
-                echo "<p>'$row["3"]'</p></div><div class=searchMileage'>";
-                echo "<"
+                echo "<p>$price</p></div><div class='searchMileage'>"; // price
+                echo "<p>$drivetrain</p></div><div class='searchLocation'>"; // drivetrain
+                echo "<p>$engine</p></div></div>"; // engine
+                echo "<div class='searchDescription'>";
+                echo "<p>$description</p></div></div><div class='searchCol rightCol'>";
+                echo "<a href='cart.php?id=$vehicleID' class='addToCart'>ADD TO CART</a><div class='numberComments'>";
+                echo "<a href='product.php#prodComment?id='$vehicleID' class='searchLink'>";
+                echo "$numComments Comments<img src='images/comment-bubble.png' class='commentBubble'></a></div></div></div>";
               }
-            }
               mysqli_free_result($results);
+            }
+            mysqli_close($connection);
           }
-          mysqli_close($connection);
         ?>
-
-
-
-        <div class="searchEntry">
-          <div class="searchCol leftCol">
-            <div class="thumbContainer">
-              <a href="product.php"><img src="images/singerThumb.jpg"></a>
-            </div>
-            <a href="product.php" class="searchLink">1988 Porsche 911 Carrera Targa</a>
-          </div>
-
-          <div class="searchCol middleCol">
-            <div class="midTopFlex">
-              <div class="searchPrice">
-                <p>$167,000</p>
-              </div>
-              <div class="searchMileage">
-                <!-- <img id="test" src="images/odometer.png"> -->
-                <p>35,859km</p>
-              </div>
-              <div class="searchLocation">
-                <p>Berlin, Germany</p>
-              </div>
-            </div>
-            <div class="searchDescription">
-              <!-- <h2>Description</h2> -->
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta, diam ut malesuada pellentesque, tellus enim efficitur eros, in pharetra enim tellus id neque. Vestibulum aliquam finibus enim, ut eleifend quam egestas non.
-                Quisque
-                consequat consectetur elit. Vivamus dapibus dolor nec diam posuere, eget ornare nisl faucibus.</p>
-            </div>
-          </div>
-
-          <div class="searchCol rightCol">
-            <a href="cart.php" class="addToCart">ADD TO CART</a>
-            <div class="numberComments">
-              <a href="product.php#prodComment" class="searchLink">7 Comments<img src="images/comment-bubble.png" class="commentBubble"></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="searchEntry">
-          <div class="searchCol leftCol">
-            <div class="thumbContainer">
-              <a href="product.php"><img src="images/rs5Thumb.jpg"></a>
-            </div>
-            <a href="product.php" class="searchLink">2017 Audi RS5</a>
-          </div>
-
-          <div class="searchCol middleCol">
-            <div class="midTopFlex">
-              <div class="searchPrice">
-                <p>$70,875</p>
-              </div>
-              <div class="searchMileage">
-                <p>12,589km</p>
-              </div>
-              <div class="searchLocation">
-                <p>Vancouver, Canada</p>
-              </div>
-            </div>
-            <div class="searchDescription">
-              <h2>Description</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta, diam ut malesuada pellentesque, tellus enim efficitur eros, in pharetra enim tellus id neque. Vestibulum aliquam finibus enim, ut eleifend quam egestas non.
-                Quisque
-                consequat consectetur elit. Vivamus dapibus dolor nec diam posuere, eget ornare nisl faucibus.</p>
-            </div>
-          </div>
-
-          <div class="searchCol rightCol">
-            <a href="cart.php" class="addToCart">ADD TO CART</a>
-            <div class="numberComments">
-              <a href="product.php#prodComment" class="searchLink">5 Comments<img src="images/comment-bubble.png" class="commentBubble"></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="searchEntry">
-          <div class="searchCol leftCol">
-            <div class="thumbContainer">
-              <a href="product.php"><img src="images/edoThumb.jpg"></a>
-            </div>
-            <a href="product.php" class="searchLink">2018 Mercedes AMG GTR</a>
-          </div>
-
-          <div class="searchCol middleCol">
-            <div class="midTopFlex">
-              <div class="searchPrice">
-                <p>$228,164</p>
-              </div>
-              <div class="searchMileage">
-                <p>1,867km</p>
-              </div>
-              <div class="searchLocation">
-                <p>Shanghai, China</p>
-              </div>
-            </div>
-            <div class="searchDescription">
-              <h2>Description</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam porta, diam ut malesuada pellentesque, tellus enim efficitur eros, in pharetra enim tellus id neque. Vestibulum aliquam finibus enim, ut eleifend quam egestas non.
-                Quisque
-                consequat consectetur elit. Vivamus dapibus dolor nec diam posuere, eget ornare nisl faucibus.</p>
-            </div>
-          </div>
-
-          <div class="searchCol rightCol">
-            <a href="cart.php" class="addToCart">ADD TO CART</a>
-            <div class="numberComments">
-              <a href="product.php#prodComment" class="searchLink">11 Comments<img src="images/comment-bubble.png" class="commentBubble"></a>
-            </div>
-          </div>
-        </div>
-
       </section>
     </div>
-
     <?php include "include/footer.php" ?>
 
   </main>
