a360ece5225e3b081e646459849be7ab18b2b964
CrZ25 <chrischeung97@gmail.com>
2018-12-03 00:32:43 -0800

    update

--- src/client/resetDatabase.php
+++ src/client/resetDatabase.php
@@ -2,7 +2,7 @@
 	include 'include/db_credentials.php';
     $connection = mysqli_connect($host, $user, $password, $database);
     $error = mysqli_connect_error();
-	$sqlSource = file_get_contents('order_sql.ddl');
+	$sqlSource = file_get_contents('database/order_sql.ddl');
 	mysqli_multi_query($connection, $sqlSource);
 	header('Location: admin.php');
 ?>
