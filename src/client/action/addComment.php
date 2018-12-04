c2da1b0af41d12a6b7c76be01c2fcf1ea0b40410
Julian Rey <julianandreasrey@gmail.com>
2018-12-04 14:41:11 -0800

    done client-server transfer for comment threads

--- src/client/action/addComment.php
+++ src/client/action/addComment.php
@@ -1,3 +1,18 @@
 <?php
+session_start();
+if (!isset($_SESSION["username"])) {
+  die();
+}
+
+$username = $_SESSION["username"];
+$title = $_POST["title"];
+$content = $_POST["content"];
+$parentid = $_POST["parentID"];
+$depth = $_POST["depth"];
+
+
+/* connect to database here */
 include "../include/db_credentials.php";
+
+
 ?>
