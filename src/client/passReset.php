eef11b3151b62172a2a7f94bbbe3173dd50ad5c8
ManicDrake <dragonzorb@gmail.com>
2018-12-05 15:27:14 -0800

    add functionality?

--- src/client/passReset.php
+++ src/client/passReset.php
@@ -6,19 +6,32 @@ if (isset($_SESSION['username'])) {
 }
 if(isset($_GET['email'])) {
     // "Confirm email is legit in system"
-    // generate token in passReset table with hashed token as the primary field and timestamp and email as colum fields 
-    $token = "<PLACEHOLDER>";
-    //send off email
-    $mail = "Your password authentication token is: ".$token;
-    $mail = wordwrap($msg,70);
-    if(mail($_GET['email'],"password Reset",$mail)) {
-        //mail successfully sent!
-    } else {
-        //Mail failed to send
+    include 'include/db_credentials.php';
+    $connection = mysqli_connect($host, $user, $password, $database);
+    $error      = mysqli_connect_error();
+    $emailAddr = $_GET["email"];
+    $sql = "SELECT username FROM User WHERE email = ?;";//if the preparation goes through and there were no errors with the upload
+    if ($statement = mysqli_prepare($connection, $sql)) {
+        // prepared statement insertion
+        mysqli_stmt_bind_param($statement, "s", $emailAddr);
+        // execute statement
+        mysqli_execute($statement);
+        mysqli_stmt_bind_result($statement, $results);
+        if($results != null) {//we have any values
+            // generate token in passReset table with hashed token as the primary field and timestamp and email as colum fields 
+            $token = "<PLACEHOLDER>";
+            //send off email
+            $mail = "Your password authentication token is: ".$token;
+            $mail = wordwrap($msg,70);
+            if(mail($_GET['email'],"password Reset",$mail)) {
+                //mail successfully sent!
+            } else {
+                //Mail failed to send
+            }
+        }    
     }
 }
 //Reset the users Password if the reset token timestamp hasn't expired and the mail matches the token
-
 ?>
 <!DOCTYPE HTML>
 
@@ -59,7 +72,7 @@ if(isset($_GET['email'])) {
                         <h3>Reset Code</h3>
                         <input name="code" type="text">
                         <h3>Account Email</h3>
-                        <input name="email" type="email">
+                        <input name="emailConfirm" type="email">
                         <h3>New Password</h3>
                         <input id="newPass" name="pass" type="password">
                         <h3>Confirm Password</h3>
