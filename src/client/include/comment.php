794ce6d6bd8254051d8c7ccfe050b0358843ce8d
ManicDrake <dragonzorb@gmail.com>
2018-12-05 19:55:43 -0800

    minor cleaning add developer implementation

--- src/client/include/comment.php
+++
@@ -1,9 +0,0 @@
-<li>
-    <div class="prodComment">
-        <div class="inlineEle username">
-            <p></p>
-        </div>
-        <h3 class="inlineEle"></h3>
-        <p></p>
-    </div>
-</li>
