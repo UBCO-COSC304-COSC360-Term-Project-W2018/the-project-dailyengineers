5e95154e41e31ab43395fd4e4fca3e174532cb07
Julian Rey <julianandreasrey@gmail.com>
2018-12-05 17:35:56 -0800

    removed the double-header from faq

--- src/client/action/newUser.php
+++ src/client/action/newUser.php
@@ -11,11 +11,11 @@ if (isset($_SERVER["REQUEST_METHOD"]) && ($_SERVER["REQUEST_METHOD"] == "POST"))
         //make connection
         $connection = mysqli_connect($host, $user, $password, $database);
         $error      = mysqli_connect_error();
-        
+
         if($connection -> connect_error) {
             die("Connection failed: " . $connection -> connect_error);
         }
-        echo "Connected to Server."; 
+        echo "Connected to Server.";
         if ($error != null) {
             $output = "<p>Unable to connect to database</p>".$error;
             exit($output);
@@ -24,17 +24,17 @@ if (isset($_SERVER["REQUEST_METHOD"]) && ($_SERVER["REQUEST_METHOD"] == "POST"))
             $username = $_POST["user"];
             $email = $_POST["email"];
             $sql = "SELECT * FROM User WHERE username='".$username."' OR email='".$email."'";
-            
+
             $results = mysqli_query($connection, $sql);
-            // echo implode("", mysqli_fetch_assoc($results)); 
+            // echo implode("", mysqli_fetch_assoc($results));
             if (mysqli_fetch_assoc($results) != null) {
             // if ($results != null) {
-                
+
                 echo "<p>User already exists with this name and/or email</p><a href='createAccount.php'>Return to user entry</a>";
             } else {
-                
+
                 mysqli_free_result($results);
-                
+
                 $sql = "INSERT INTO User (username, password, email) VALUES (?, ?, ?)";
                 //if the preparation goes through and there were no errors with the upload
                 if ($statement = mysqli_prepare($connection, $sql)) {//() && $uploadOk
@@ -53,18 +53,18 @@ if (isset($_SERVER["REQUEST_METHOD"]) && ($_SERVER["REQUEST_METHOD"] == "POST"))
                         $results = mysqli_query($connection, $sql);
                         $userID =  implode("", mysqli_fetch_assoc($results));
                         // $imagedata = file_get_contents($_FILES['profile']['tmp_name']);
-                    
+
                         $sql = "INSERT INTO Customer (userID, firstName, lastName) VALUES(?, ?, ?);";
-                    
+
                         $stmt = mysqli_stmt_init($connection); //init prepared statement object
-                    mysqli_stmt_prepare($stmt, $sql); // register the query
-                    $null = null;
+                        mysqli_stmt_prepare($stmt, $sql); // register the query
+                        $null = null;
                         mysqli_stmt_bind_param($stmt, "iss", $userID, $_POST['firstName'], $_POST['lastName']);
                     
                         // mysqli_stmt_send_long_data($stmt, 2, $imagedata);
-                    
+
                         $result = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
-                    
+
                         mysqli_stmt_close($stmt); // and dispose of the statement.
                         mysqli_close($connection);
 
